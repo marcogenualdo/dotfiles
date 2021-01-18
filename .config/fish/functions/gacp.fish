81c4d06e1d7a76840536f8591e8cf5584dd7d97b
mgenualdo <marco.genualdo@alten.it>
2021-01-18 22:34:43 +0100

    Fish: gacp function args. Tmux: new panes open in current folder, statusbar.

--- .config/fish/functions/gacp.fish
+++ .config/fish/functions/gacp.fish
@@ -1,8 +1,8 @@
 function gacp --description "git add --all, commit and push"
     git add --all
     git commit -m $argv[1]
-    if set -q argv[2] 
-    	git push $argv[2]
+    if set -q argv[3]
+    	git push $argv[2] $argv[3]
     else
 	git push
     end
