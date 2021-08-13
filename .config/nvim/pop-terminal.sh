fd53af787b00502404ba80461c972c6d9be6b39b
mgenualdo <marco.genualdo@alten.it>
2021-08-14 00:18:55 +0200

    Vim: pop terminal script circles back to original session if executed from terminal session.

--- .config/nvim/pop-terminal.sh
+++ .config/nvim/pop-terminal.sh
@@ -5,7 +5,12 @@ THIS_SESSION=$(tmux display-message -p '#S')
 THIS_WINDOW=$(tmux display-message -p '#I')
 
 # terminal session
-TERM_SESSION=$THIS_SESSION-term
+if [ $(echo $THIS_SESSION | grep -o '.....$') == '-term' ];then
+    TERM_SESSION=${THIS_SESSION::-5}
+else
+    TERM_SESSION=$THIS_SESSION-term
+fi
+
 tmux new-session -ds $TERM_SESSION
 tmux new-window -t $TERM_SESSION:$THIS_WINDOW
 
